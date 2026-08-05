1e26d42916f6ac5d6f36bf3cd245d19d5785bfff
Hiruni Jayawardena <hiruniejayawardena@gmail.com>
2026-08-05 23:55:55 +0530

    task 8 added

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ console.log(petDog);
 console.log(petCat);
 var petDog = "Abby";
 console.log("My pet dog's name is " + petDog);
+var catSound = "purr";
+var dogSound = "woof";
+console.log(petDog, "says", dogSound);
