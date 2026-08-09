4cc306717814d43f54b141c8b567324e49e184d1
Hiruni Jayawardena <hiruniejayawardena@gmail.com>
2026-08-09 23:58:31 +0530

    Number

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-console.log(2 + 2);
-console.log(1 + 2 + 3 + 4 + 5);
-console.log(20 - 18);
-console.log(2 * 3);
-console.log(8 / 1);
-console.log (8 / 2);
-console.log(3 > 2);
-console.log(2 > 3);
-console.log(10 == 10);
-console.log(10 != 10);
+123
